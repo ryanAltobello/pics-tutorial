6c9728475e75166e3813cc236b0d2b2299f71661
Ryan Altobello <rchap504@gmail.com>
2019-02-02 11:20:47 -0600

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
     Authorization:
-      "Client-ID 7e6e0d466563e6251a97c32442c68a6069017ae00b214030f2d374685d835dbf"
+      "Client-ID {API Key}"
   }
 });
