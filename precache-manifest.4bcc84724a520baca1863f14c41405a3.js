571942f3382d5bd182450821e769bccc260fd080
ryanAltobello <rchap504@gmail.com>
2019-02-22 16:15:33 -0600

    Updates

---
+++ precache-manifest.4bcc84724a520baca1863f14c41405a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8877d05aa5898c71acd9",
+    "url": "/pics-tutorial/static/js/runtime~main.8877d05a.js"
+  },
+  {
+    "revision": "b59a038b41c84b1f465a",
+    "url": "/pics-tutorial/static/js/main.b59a038b.chunk.js"
+  },
+  {
+    "revision": "7554df4382c4c5087abd",
+    "url": "/pics-tutorial/static/js/2.7554df43.chunk.js"
+  },
+  {
+    "revision": "b59a038b41c84b1f465a",
+    "url": "/pics-tutorial/static/css/main.b207ae20.chunk.css"
+  },
+  {
+    "revision": "829a9ded8004555119230806ac11d4dc",
+    "url": "/pics-tutorial/index.html"
+  }
+];
